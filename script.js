6cfbcb5d6bf40e15f3965871350810fd0470e6f9
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-18 18:42:44 -0400

    Able to display a single book object from the myLibrary array

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log("hello world!")
 
+
+const table = document.querySelector(".table")
+const newRow = document.createElement("tr");
+const author = document.createElement("td");
+const title = document.createElement("td");
+const pages = document.createElement("td");
+const read = document.createElement("td");
+
 const myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -12,10 +20,13 @@ function Book(author, title, pages, read) {
 
 
 const tbp = new Book("Cixin Liu", "Three Body Problem", 528, true);
-const hmp = new Book("Andy Weir", "Project Hail Mary", 488, true);
+const phm = new Book("Andy Weir", "Project Hail Mary", 488, true);
 const childrenOfMem = new Book("Adrian Tchaikovsy", "Children of Memory", 467, false);
 
-myLibrary.push(tbp, hmp, childrenOfMem);
+myLibrary.push(phm);
+myLibrary.push(tbp);
+myLibrary.push(childrenOfMem);
+
 
 function addBookToLibrary(book, author, title, pages, read) {
     newBook = new book(author, title, pages, read)
@@ -24,11 +35,36 @@ function addBookToLibrary(book, author, title, pages, read) {
 
 
 
-// function that loops through the mylibrary array and outputs the value of the key
-function testFunction(key) {
-    for (let value in key) {
-        console.log("\n" + key[value])
-    }
-}
+// function that loops through mylibrary array and outputs the value of the key
+// function testFunction(key) {
+//     for (let value in key) {
+//         document.writeln(key[value]);
+//     }
+// }
 
-myLibrary.forEach(testFunction);
+// myLibrary.forEach(testFunction);
+
+
+myLibrary.forEach((element) => {
+    
+    //append the author to the table
+    author.textContent = element.author;
+    newRow.appendChild(author);
+    table.appendChild(newRow);
+    
+    //append the title to the table
+    title.textContent = element.title;
+    newRow.appendChild(title);
+    table.appendChild(newRow);
+
+    //append to the page count to the table
+    pages.textContent = element.pages;
+    newRow.appendChild(pages);
+    table.appendChild(newRow);
+
+    //append if read or not to the table
+    read.textContent = element.read;
+    newRow.appendChild(read);
+    table.appendChild(newRow);
+});
+
